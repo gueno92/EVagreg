1b3c2103023b08808c1aa3a634be4ead16f7e511
Adrien Guenard <adrienguenard@MacBook-Pro-de-Adrien-2.local>
2024-06-19 18:03:33 +0200

    Working App

--- EV_class.py
+++ EV_class.py
@@ -3,6 +3,8 @@ import numpy as np
 from gurobipy import Model, GRB, quicksum
 import matplotlib.pyplot as plt
 import random
+from datetime import datetime, timedelta
+
 class EV_BATTERY:
     
     def __init__(self, P_charg: float,
@@ -18,6 +20,7 @@ class EV_BATTERY:
         capacity (float) : battery storage capacity
         P_dischar (float) : discharge capacity
         """
+        np.random.seed(0)
         self.P_charg = P_charg
         self.capacity = capacity
         self.P_dischar = P_dischar
@@ -25,24 +28,58 @@ class EV_BATTERY:
         self.n_EV = n_EV
 
 
-    def read_spot_price(self, path_to_price : str):
+    def read_spot_price(self, path_to_price : str, 
+                        resample = False):
         """
         Read the df of price
         """
         df_price = pd.read_csv(path_to_price)
 
         self.price_ls = df_price['Prix (€/MWh)']
+        self.delta_T =1
+        self.time_step = 60
+        if resample :
+            self.delta_T = 1/6
+            self.time_step = 60*self.delta_T
+            resampled_list = []
+            for elem in self.price_ls:
+                resampled_list.extend([elem] * 6)
+            self.price_ls = resampled_list
 
         self.T = len(self.price_ls)
 
+    def reformate_charing_time(self,
+                               charging_schedule):
+        """
+        Reformate the charging schedule to make it suitable with 
+        the optimization process
+
+        Args : 
+        charging_schedule : tuple of datetime object of format "MM/DD HH:mm"
+        """
+        start_price_date = datetime(2024, 6, 19, 18, 00)
+        #Determine the start position for the optimization
+        detla_time_start = charging_schedule[0] - start_price_date
+        # for now the steps are of 10 minutes
+        start_pos = int(detla_time_start.total_seconds()/(60*self.time_step))
 
 
-    def generate_contiguous_list(self):
+        # Determine the charging duration 
+        detla_time_charge = charging_schedule[1] -  charging_schedule[0]
+        duration_charging = int(detla_time_charge.total_seconds()/(60*self.time_step))
+
+        # Store everything in the object
+        self.len_ones = duration_charging
+        self.start_pos = start_pos
+
+
+    def generate_contiguous_list(self,
+                                 random: bool = True):
         """
         Generates a list with contiguous blocks of 1s and 0s where the series of 1s starts at a random position and has a random length.
         
         Parameters:
-        length (int): Length of the list.
+        random (bool): Have random time of stay or not.
         
         Returns:
         list: The generated list.
@@ -51,45 +88,68 @@ class EV_BATTERY:
         if length <= 1:
             raise ValueError("Length of the list must be greater than 1.")
         
-        # Determine the length of the series of 1s
-        len_ones = random.randint(1, length - 1)
+        if random : 
+            # Determine the length of the series of 1s
+            self.len_ones = random.randint(1, length - 1)
+            # Determine the starting position for the series of 1s
+            self.start_pos = random.randint(0, length - self.len_ones)
         
-        # Determine the starting position for the series of 1s
-        start_pos = random.randint(0, length - len_ones)
+        else : 
+            print('Not random')
+
         
         # Generate the list with contiguous blocks of 1s and 0s
         ls = [0] * length
-        for i in range(len_ones):
-            ls[start_pos + i] = 1
-        
+        for i in range(self.len_ones):
+            ls[self.start_pos + i] = 1
         return ls
 
-    def generate_contiguous_matrix(self):
+    def generate_contiguous_matrix(self,
+                                   SOC_random:bool = True,
+                                   SOC_ini = None,
+                                   SOC_final = None):
         """
         Generates a matrix with contiguous blocks of 1s and 0s in each column.
         
         Parameters:
-        rows (int): Number of rows in the matrix.
-        cols (int): Number of columns in the matrix.
+        SOC_random (bool): If random the initial random 
+        SOC_ini (np.array): Initial state of charge
+        SOC_final (np.array) : desired final state of charge
         
-        Returns:
-        np.ndarray: The generated matrix.
         """
         rows = self.T
         cols = self.n_EV
         matrix = np.zeros((rows, cols), dtype=int)
         
-        for col in range(cols):
-            matrix[:, col] = self.generate_contiguous_list()
-        
+        # Create random SOC and Schedule
+        if SOC_random:
+            self.SOC_ini = np.random.uniform(low=0.0, high=1.0, size= (self.n_EV,))
+            self.SOC_final = np.ones((self.n_EV,))
+
+            for col in range(cols):
+                matrix[:, col] = self.generate_contiguous_list()
+
+        # Create SOC and schedule based on inputs
+        else : 
+            self.SOC_ini =  SOC_ini
+            self.SOC_final =  SOC_final
+
+            for col in range(cols):
+                matrix[:, col] = self.generate_contiguous_list(random=False)
+
+
         self.schedule_matrix = matrix
 
 
 
-    def battery_dispatch(self):
+    def battery_dispatch(self,
+                         charging_type:str = 'VPP'):
         """
         Optimize the battery dispatch using the price collected
         from RTE
+
+        Args:
+        charging_type (str) : smart, VPP, fast 
         """
         #Time over which to dispatch
         T = len(self.price_ls)
@@ -103,40 +163,53 @@ class EV_BATTERY:
         x_dch = model.addVars(T,self.n_EV, lb=0.0, vtype=GRB.CONTINUOUS, name="Discharge") # Discharge from the battery
         x_soc = model.addVars(T, self.n_EV, lb=0.0, ub = 1, vtype=GRB.CONTINUOUS, name="SOC") # State of charge of battery
         x_dispatch = model.addVars(T, self.n_EV, vtype=GRB.BINARY, name="dispatch") # Decision on dispatch (0 => no dispatch, 1 => dispatch)
-
+        
 
         # Objective function: Maximize the revenue of the battery
-        model.setObjective(quicksum(quicksum((x_dch[i,k] - x_ch[i,k])*self.price_ls[i] 
-                                             for i in range(T)) 
-                                             for k in range(self.n_EV)), GRB.MAXIMIZE)
+        if charging_type == 'VPP':
+            model.setObjective(quicksum(quicksum((x_ch[i,k] - x_dch[i,k]*self.eta)*self.price_ls[i]*self.delta_T*10**-3
+                                                for i in range(T)) 
+                                                for k in range(self.n_EV)), GRB.MINIMIZE)
+        elif charging_type == 'smart':
+            model.setObjective(quicksum(quicksum((x_ch[i,k])*self.price_ls[i]*self.delta_T*10**-3
+                                    for i in range(T)) 
+                                    for k in range(self.n_EV)), GRB.MINIMIZE)
+
+        elif charging_type == 'fast':
+            model.setObjective(quicksum(quicksum(((x_soc[i,k]))
+                                    for i in range(T)) 
+                                    for k in range(self.n_EV)), GRB.MAXIMIZE)
 
 
         # Constraints (1)
         # Cannot discharge more than the available energy
         for t in range(T):
             for k in range(self.n_EV):
-                model.addConstr(x_dch[t,k] <= x_dispatch[t,k]*x_soc[t,k]*self.capacity * self.schedule_matrix[t,k])
+                model.addConstr(x_dch[t,k] <= x_dispatch[t,k]*x_soc[t,k]*self.capacity * self.schedule_matrix[t,k]/self.delta_T)
                 model.addConstr(x_dch[t,k] <= x_dispatch[t,k]*self.P_dischar * self.schedule_matrix[t,k])
         
 
-        # Constraints (3)
+        # Constraints (2)
         # Cannot charge more than available and cannot charge while dispatching
         for t in range(T): 
             for k in range(self.n_EV):
-                model.addConstr(x_ch[t,k] <= (1-x_dispatch[t,k])*(1-x_soc[t,k])*self.capacity * self.schedule_matrix[t,k])
+                model.addConstr(x_ch[t,k] <= (1-x_dispatch[t,k])*(1-x_soc[t,k])*self.capacity * self.schedule_matrix[t,k]/self.delta_T)
                 model.addConstr(x_ch[t,k] <= (1-x_dispatch[t,k])*self.P_charg * self.schedule_matrix[t,k])
 
-        # Constraint (3)
+
+
+        # Constraint (4)
         # Create the state of charge variable (start with full)
         # Start with a charged battery
         for k in range(self.n_EV):
-            model.addConstr(x_soc[0,k] == np.random.uniform(low=0.0, high=1.0))
-            model.addConstr(x_soc[T-1,k] >= 0.95)
+            model.addConstr(x_soc[0,k] == self.SOC_ini[k])
+            if np.sum(self.schedule_matrix[:,k])>5:
+                model.addConstr(x_soc[T-1,k] >= self.SOC_final[k])
         
         # State of charge at each time step
         for t in range(1, T):
             for k in range(self.n_EV):
-                model.addConstr((x_soc[t,k] - x_soc[t-1,k])*self.capacity == x_ch[t-1,k] - x_dch[t-1,k])
+                model.addConstr((x_soc[t,k] - x_soc[t-1,k])*self.capacity == (x_ch[t-1,k] - x_dch[t-1,k])*self.delta_T)
 
 
         #Solve the model
@@ -156,5 +229,46 @@ class EV_BATTERY:
                 discharge[j,k] = x_dch[j,k].X
                 SOC[j,k] = x_soc[j,k].X
 
+        if charging_type in ['VPP', 'smart']:
+            obj = model.getObjective()
+            self.obj_value = obj.getValue()
+            print(f'Objective value for {charging_type} = {self.obj_value}')
+        else : 
+            self.obj_value = 0
+            for k in range(self.n_EV):
+                self.obj_value += np.sum(charge[:,k]*self.price_ls[:]*self.delta_T*10**-3)
+            print(f'Objective value for {charging_type} = {self.obj_value}')
 
+        self.SOC = SOC
         return charge, discharge, SOC
+    
+
+
+    def generate_result_df(self):
+        """
+        Method that combine all the results in a dataframe
+        The dataframe can be used for data visualization purpose
+        """
+
+        start_price_date = datetime(2024, 6, 19, 18, 00)
+        end_price_date = datetime(2024, 6, 20, 17, 50)
+
+        # Create the list of data
+        date_list = []
+        current_date = start_price_date
+
+        while current_date <= end_price_date:
+            date_list.append(current_date)
+            current_date += timedelta(minutes=10)
+
+
+        # Create the initial df with the SOC
+
+        columns_name = [f'SOC {i}' for i in range(self.n_EV)]
+        df = pd.DataFrame(self.SOC, columns=columns_name)
+
+        # Add the price and the time
+        df['Price [$/MWh]'] = self.price_ls
+        df['Time'] = date_list
+
+        return df
---
+++ dashboard.py
@@ -0,0 +1,136 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import time
+from datetime import datetime, timedelta
+from EV_class import EV_BATTERY
+import plotly.express as px
+import plotly.graph_objects as go
+
+# Set the page layout
+st.set_page_config(layout="wide")
+
+# Sidebar for input sliders
+st.sidebar.title("User Input")
+
+# State of charge data
+SOC_ini = st.sidebar.slider(f'Initial State of Charge', 0, 100, 50)
+SOC_final = st.sidebar.slider(f'Desired State of Charge', 0, 100, 95)
+    
+
+start_time = datetime.now().replace(hour=18, minute=0, second=0, microsecond=0)
+end_time = start_time + timedelta(days=1, hours=-1)
+
+# Create the slider charging schedule timeline
+charging_schedule = st.sidebar.slider(
+    "Schedule your charging schedule:",
+    min_value=start_time,
+    max_value=end_time,
+    value=(start_time + timedelta(hours=0, minutes=0), start_time + timedelta(hours=5, minutes=45)),
+    step=timedelta(minutes=10),
+    format="MM/DD HH:mm"
+)
+
+#=====================================================================================
+#============= Run simulation ========================================================
+#=====================================================================================
+
+Batt = EV_BATTERY(P_charg=46, capacity=62, P_dischar=7, eta = 0.8, n_EV=3)
+
+Batt.read_spot_price('Price_data/bourseEpex_06_06_2024.csv', resample=True)
+Batt.reformate_charing_time(charging_schedule)
+
+print(Batt.start_pos)
+print(Batt.len_ones)
+Batt.generate_contiguous_matrix(SOC_random=False,
+                                         SOC_ini= SOC_ini/100*np.ones((3,)),
+                                         SOC_final= SOC_final/100*np.ones((3,)))
+charge, discharge, SOC = Batt.battery_dispatch('VPP')
+# Generate the df only for the VPP
+df_VPP = Batt.generate_result_df()
+
+total_price= Batt.obj_value
+
+#Look at the other benchmark
+total_price_ls = []
+charge, discharge, SOC = Batt.battery_dispatch('VPP')
+total_price_ls.append(Batt.obj_value)
+charge_smart, discharge_smart, SOC_smart = Batt.battery_dispatch('smart')
+total_price_ls.append(Batt.obj_value)
+charge_fast, discharge_fast, SOC_fast = Batt.battery_dispatch('fast')
+total_price_ls.append(Batt.obj_value)
+
+
+# Main dashboard layout
+col1, col2 = st.columns(2)
+
+# Top-left: Text and number
+col1.subheader("Charging operation for Nissan LEAF")
+col1.metric(label="Charging cost [$]", value=np.round(total_price,2))
+col1.metric(label="Initial SOC [%]", value=SOC_ini)
+col1.metric(label="Desired SOC [%]", value=SOC_final)
+
+# Top-right: Bar plot
+col2.subheader("Comparison of the charging method")
+#fig, ax = plt.subplots(figsize = (4,3), dpi = 300)
+#bars = ax.bar(['VPP', 'Smart', 'Fast'], total_price_ls)
+#ax.set_ylim(np.min(total_price_ls)*1.15, np.max(total_price_ls)*1.15)
+# Adding annotations
+#for bar, value in zip(bars, total_price_ls):
+  #  height = np.round(bar.get_height(),2)
+  #  print(height)
+ #   ax.text(bar.get_x() + bar.get_width() / 2.0, height, f'{np.round(value,2)}', ha='center', va='bottom')
+#ax.set_ylabel('Price [$]')
+
+
+data_price = pd.DataFrame({'Price [$]' : total_price_ls, 'Method': ['V2G', 'Smart', 'Fast']})
+fig = px.bar(data_price, x='Method', y='Price [$]')
+
+#col2.pyplot(fig)
+col2.plotly_chart(fig)
+
+# Bottom: Line plot
+st.subheader("Charging profile")
+
+
+# Create figure
+fig = go.Figure()
+
+# Add scatter plot for 'Price [$/MWh]'
+fig.add_trace(
+    go.Scatter(x=df_VPP['Time'], y=df_VPP['Price [$/MWh]'], name="Price [$/MWh]", mode='lines+markers', yaxis="y1")
+)
+
+# Add scatter plot for 'SOC'
+fig.add_trace(
+    go.Scatter(x=df_VPP['Time'], y=df_VPP['SOC 0'], name="SOC", mode='lines+markers', yaxis="y2")
+)
+
+# Create axis objects
+fig.update_layout(
+    xaxis=dict(
+        title="Time"
+    ),
+    yaxis=dict(
+        title="Price [$/MWh]",
+        titlefont=dict(color="#1f77b4"),
+        tickfont=dict(color="#1f77b4")
+    ),
+    yaxis2=dict(
+        title="SOC",
+        titlefont=dict(color="#ff7f0e"),
+        tickfont=dict(color="#ff7f0e"),
+        anchor="x",
+        overlaying="y",
+        side="right"
+    )
+)
+
+# Update layout
+fig.update_layout(
+    legend=dict(x=0, y=1),
+    margin=dict(l=40, r=40, t=40, b=40)
+)
+
+event = st.plotly_chart(fig)
